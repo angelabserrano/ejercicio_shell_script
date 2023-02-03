948f0d1016fc68db404df7ff564ba64587f77c90
angelabserrano <61191992+angelabserrano@users.noreply.github.com>
2023-02-03 10:19:23 +0100

    Update ej1.sh
    Corrección de fallo de sintaxis. El programa no ejecutaba bien

--- ej1.sh
+++ ej1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-num1= $1
-num2= $2
+num1=$1
+num2=$2
 
-if [$1 -gt $2]; then
+if [ $1 -gt $2 ]; then
 	echo "El primer numero es mayor que el segundo "
-   elif [$1 -lt $2]; then
+   elif [ $1 -lt $2 ]; then
 	echo "El primer numero es menor que el segundo "
    else
 	echo "Los 2 numeros son iguales"
